fcf00b9d3825f630d29420a9c70893459fa1a730
hoantqph45149 <hoantqph45149fpt.edu.vn>
2024-05-30 20:06:08 +0700

    Lesson 11: validation

--- src/pages/admin/ProductAdd.jsx
+++ src/pages/admin/ProductAdd.jsx
@@ -3,52 +3,78 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 
-/**
- * ! BTVN:
- * 1. validation title required, it nhat 6 ky tu
- * 2. validation price required, >=0
- */
+const schema = z.object({
+  title: z.string().min(6).nonempty(),
+  price: z.number().min(0),
+  description: z.string().min(10).nonempty(),
+});
 
 const ProductAdd = ({ onAddProduct }) => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm();
-	const onSubmit = (data) => {
-		console.log(data);
-		onAddProduct(data);
-	};
-	return (
-		<>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<h1>Product Add</h1>
-				<div className="mb-3">
-					<label htmlFor="title" className="form-label">
-						Title
-					</label>
-					<input type="text" className="form-control" id="title" {...register("title")} />
-				</div>
-				<div className="mb-3">
-					<label htmlFor="price" className="form-label">
-						Price
-					</label>
-					<input type="text" className="form-control" id="price" {...register("price")} />
-				</div>
-				<div className="mb-3">
-					<label htmlFor="description" className="form-label">
-						Description
-					</label>
-					<input type="text" className="form-control" id="description" {...register("description")} />
-				</div>
-				<div className="mb-3">
-					<button type="submit" className="btn btn-primary w-100">
-						Add product
-					</button>
-				</div>
-			</form>
-		</>
-	);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    onAddProduct(data);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Product Add</h1>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            {...register("title")}
+          />
+          {errors.title && (
+            <span className="text-danger">{errors.title.message}</span>
+          )}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="price" className="form-label">
+            Price
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="price"
+            {...register("price")}
+          />
+          {errors.price && (
+            <span className="text-danger">{errors.price.message}</span>
+          )}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="description"
+            {...register("description")}
+          />
+          {errors.description && (
+            <span className="text-danger">{errors.description.message}</span>
+          )}
+        </div>
+        <div className="mb-3">
+          <button type="submit" className="btn btn-primary w-100">
+            Add product
+          </button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default ProductAdd;
